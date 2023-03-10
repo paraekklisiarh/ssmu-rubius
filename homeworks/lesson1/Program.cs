a8441e6598adf21665d18377e0dae00cb50b5aaf
Paraekkl <paraekklisiarh@gmail.com>
2023-03-10 22:48:13 +0700

    Lesson 1 + gitignore created
    Попытка проектирования репозитория и д/з №1.

---
+++ homeworks/lesson1/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+
+{
+    public static void Main()
+    {
+
+        LessonOne.Hello();
+        
+    }
+}
+
+public class LessonOne
+{
+    public static void Hello()
+    {
+        Console.WriteLine("Введите имя:");
+        
+        var name = Console.ReadLine();
+        
+        Console.WriteLine(name + ", привет!");
+
+    }
+}

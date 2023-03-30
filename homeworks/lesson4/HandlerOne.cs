f6e1d0d70f423b30c166a4f7550d69d4775e892a
Paraekkl <paraekklisiarh@gmail.com>
2023-03-30 15:09:27 +0700

    Lesson 4: event and how it handle

---
+++ homeworks/lesson4/HandlerOne.cs
@@ -0,0 +1,9 @@
+namespace lesson4;
+
+public class HandlerOne
+{
+    public void Message(int count)
+    {
+        Console.WriteLine($"Уже {count}, давно пора было начать!");
+    }
+}
---
+++ homeworks/lesson4/HandlerTwo.cs
@@ -0,0 +1,9 @@
+namespace lesson4;
+
+public class HandlerTwo
+{
+    public void Message(int count)
+    {
+        Console.WriteLine($"Пора действовать, ведь уже {count}");
+    }
+}
